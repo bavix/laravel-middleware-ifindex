cbb1b4a375670f9dc6a77bc956fb124115879904
Babichev Maxim <info@babichev.net>
2017-09-07 20:36:51 +0300

    first version

---
+++ src/Middleware/IfIndex.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+
+class IfIndex
+{
+
+    public function handle(Request $request, \Closure $next)
+    {
+        $uri  = $request->server('REQUEST_URI');
+        $name = $request->server('SCRIPT_NAME');
+
+        if (strpos($uri, $name) !== false)
+        {
+            $path  = $request->path();
+            $query = $request->getQueryString();
+
+            if (null !== $query)
+            {
+                $path .= '?' . $query;
+            }
+
+            // disabled laravel redirect
+            header('location: ' . $path, true, 301);
+            die;
+        }
+
+        return $next($request);
+    }
+
+}
